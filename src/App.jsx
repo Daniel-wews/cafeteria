a85fae8c1f078e9c8cc06e630bc379b39e1cb3f9
Daniel Junior <daniel1236100@gmail.com>
2024-07-08 10:43:17 -0300

    secao de informacoes criada.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Banner from "./components/banner/banner";
 import BomCafe from "./components/bomcafe/bomcafe";
 import Header from "./components/header/header";
 import Informacao from "./components/informacao/informacao";
+import Informacoes from "./components/informacoes/informacaoes";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Banner/>
       <Informacao/>
       <BomCafe/>
+      <Informacoes/>
     </>
   )
 }
---
+++ src/components/informacoes/informacaoes.jsx
@@ -0,0 +1,16 @@
+// Informacoes.js
+
+import React from 'react';
+
+function Informacoes() {
+  return (
+    <div className="bg-white w-11/12 md:w-9/12 mx-auto py-10 text-center">
+      <h2 className="text-xl font-bold text-yellow-600 mt-8 mb-4">O QUE É O CAFÉ MANHATTAN?</h2>
+      <p className="text-base leading-6 text-black  italic font-serif">
+        Segundo a Metodologia de Avaliação Sensorial da SCA (Specialty Coffee Association), usada no mundo todo, um café especial é todo aquele que atinge, no mínimo, 80 pontos na escala de pontuação da metodologia (que vai até 100). O Café Manhattan destaca-se nos atributos fragrância, corpo, harmonia, doçura e sabor, nesse caso, possuindo sabores e aromas que podem ser frutados, herbais, doces como caramelo e chocolate. Podemos fazer uma analogia com o vinho, pois o café Manhattan também pode ser apreciado por suas características sensoriais e não apenas pela cafeína. Através do excelente padrão de cultivo, cuidado pós-colheita e da torra, você poderá experimentar diferentes e deliciosos tipos de café.
+      </p>
+    </div>
+  );
+}
+
+export default Informacoes;
